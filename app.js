70d97c66721892457108f4b884fce659ed021885
Ahmed-Shahi <ahmedshahi038@gmail.com>
2023-09-15 04:02:31 +0500

    not done

---
+++ app.js
@@ -0,0 +1,341 @@
+// 1
+
+// var a = 'hello world'
+// for(var i = 1 ; i<=5 ; i++){
+//     document.write(a+ '<br>')
+// }
+
+// 1
+
+// 2
+
+// for(var i = 1 ; i<=10 ; i++){
+//     document.write(i+ '<br>')
+// }
+
+// 2
+
+// 3
+
+// var a = +prompt('Enter a Table No. ');
+// var b = +prompt('Enter a starting No. ');
+// var c = +prompt('Enter a ending No. ');
+// var d;
+
+
+// for(d=b; d<=c; d++){
+//     document.write(  a ,' x ',d,' = ', a*d, '<br>');
+
+// }   
+
+// 3
+
+// 4
+
+// var a =['ahmed','usman','saad','shahi']
+
+// for(var i = 0 ; i<=a.length ; i++){
+//     document.write(a[i]+'<br>')
+// }
+
+
+// 4
+
+// 5
+
+// var a =['ahmed','usman','saad','shahi']
+
+// for(var i = 0 ; i<=a.length  ; i++){
+//     document.write('Element at '+i+' index is '+a[i]+'<br>')
+// }
+
+// 5
+
+// 6
+
+// var p = +prompt('Enter a number')
+
+// if(p>0){
+//   var  a = []
+//   for(var i = 0 ; i < p ; i++){
+//     var p2 = prompt('enter '+ (1+i)+ ' items')
+//     a.push(p2)
+//   }
+
+// }
+
+// 6
+// 7
+
+// document.write('counting'+'<br>')
+// for(var i = 1 ; i<=5 ; i++){
+//     document.write(  i+ '<br>')
+// }
+
+// document.write('reverse'+'<br>')
+// for(var j =10 ; j>=1 ; j--){
+//     document.write(  j+ '<br>')
+// }
+
+// document.write('odd'+'<br>')
+// for(var i = 1 ; i<=10 ; i+=2){
+//     document.write(  i + '<br>')
+// }
+
+
+// document.write('even'+'<br>')
+// for(var i = 2 ; i<=10 ; i+=2){
+//     document.write(  i + '<br>')
+// }
+
+// document.write('series'+'<br>')
+// for(var i = 2 ; i<=20 ; i+=2){
+//     document.write(  i +'k' +'<br>')
+// }
+
+// 7
+
+// 8
+
+// var p = prompt('enter a name')
+// var a =['ahmed','usman','saad','shahi']
+// var match = false
+// for(var i = 0 ; i<=a.length  ; i++){
+//     if(a[i]== p ){
+//         match = true
+//         alert('yes....')
+//         break
+//     }
+
+//     if(match == false){
+//         alert('no....')
+
+//     }
+// }
+
+// 8
+
+// 9
+
+// var a = [23,80,68,54,58,55]
+// var a2 =[0]
+// for( var i = 0 ; i<=a.length ; i++){
+//     if(a[i]>a2){
+//         a2 = a[i]
+//     }
+// }
+// document.write(a2)
+
+// 9
+
+// 10
+
+
+// var a = [23,80,68,54,4,55]
+// var a2 =[1/0]
+// for( var i = 0 ; i<=a.length ; i++){
+//     if(a[i]<a2){
+//         a2 = a[i]
+//     }
+// }
+// document.write(a2)
+
+
+
+// 10
+
+// 11
+
+// var a = [23,80,68,54,4,55]
+// var a2 =[1/0]
+// for( var i = 0 ; i<=a.length ; i++){
+//     if(a[i]<a2){
+//         a2 = a[i]
+//     }
+// }
+// document.write(a2,'<br>')
+// var a3 =[0]
+// for( var i = 0 ; i<=a.length ; i++){
+//     if(a[i]>a3){
+//         a3 = a[i]
+//     }
+// }
+// document.write(a3)
+
+// 11
+
+// 12
+
+// for(var i = 1 ; i<=20 ; i++ ){
+//     document.write(5*i+',')
+// }
+
+// 12
+
+// 13
+
+// var n =['ahmed','usman','saad','shahi']
+// var s = [23,80,68,54]
+
+// document.write('<table border="2px">'
+// +'<tr>'+'<td>'+n[0]+'</td>'+'<td>'+s[0] +'</td>'+'</tr>'
+// +'<tr>'+'<td>'+n[1]+'</td>'+'<td>'+s[1] +'</td>'+'</tr>'
+// +'<tr>'+'<td>'+n[2]+'</td>'+'<td>'+s[2] +'</td>'+'</tr>'
+// +'<tr>'+'<td>'+n[3]+'</td>'+'<td>'+s[3] +'</td>'+'</tr>'
+// +'</table>')
+
+// 13
+
+// 14
+
+// var a =[22,58,69,14,54,75]
+// var p = +prompt('Enter a value')
+
+// // for(var i in a){
+// //     if(p==a[i]){
+// //         document.write(a)
+// //     }
+// // }
+// for(var i = 0 ; i<=a.length  ; i++){
+//     if(p == a[i] ){
+//      var  d =   document.write(a.indexOf(p))
+//     var b = a.slice(d,1/0)
+//     document.write(b)   
+
+// }
+
+// }
+
+// 14
+
+// 15
+
+// var a = [[1,2,3],[4,5,6],[7,8,9]]
+// for(var i in a){
+//     for(var j in a){
+//         document.write(a[i][j]+'<br>')
+//             }
+//     }
+
+// 15
+
+// 16
+
+var p = +prompt('enter a value')
+if(p>=1){
+    for(var i = p ; i>p ; i-- ){
+    }
+    document.write(i+ '<br>')
+
+}
+
+// 16
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
